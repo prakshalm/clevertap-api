31f1d3baea8ff5fcf808db4c2f2eb2f5c3312b7d
Prakshal Mehra <prakshalm@ip-10-5-50-190.ap-south-1.compute.internal>
2022-04-25 11:50:23 +0530

    upd: records for 5min ts added

--- dryRun.py
+++ dryRun.py
@@ -1,10 +1,26 @@
-# endpoint= http://6b95-203-189-255-67.ngrok.io
-
-from unittest import result
 from main import EventsAPIManager
-
+import sys
 obj=EventsAPIManager('cx_app')
-cursor=obj.get_event_cursor()
-res=obj.get_records_for_cursor(cursor)
-print(res)
-
+from_date = int(sys.argv[1])
+to_date = int(sys.argv[2])
+if len(sys.argv)<4:
+    cursor=obj.get_event_cursor(from_date, to_date)
+    res=obj.get_records_for_cursor(cursor)
+    if res is None:
+        print("no data")
+    else:
+        data_list = res['records']
+        print(data_list)
+   
+else:
+    from_time = int(sys.argv[3])
+    to_time = int(sys.argv[4])
+    cursor=obj.get_event_cursor(from_date, to_date)
+    res=obj.get_records_for_cursor(cursor)
+    if res is None:
+        print("no data")
+    else:
+        data_list = res['records']
+        for idx,data in enumerate(data_list):
+            if int(from_time) <= data_list[idx]["ts"] <= int(to_time):
+                print(data)
